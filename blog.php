8ddb65b9394ab7935a73e7db2f6a078b48351484
Tony Homfray <tonyhomfray@gmail.com>
2019-05-17 21:12:21 +0100

    Adds the outline of blog.php.

---
+++ blog.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>CV</title>
+</head>
+<body>
+    <div class="grid-wrapper">
+
+        <?php include("includes/header-footer.php"); ?>
+
+        <div id="main_section">
+            <h1>Blog</h1>
+            <p>This blog will chart the development of this site and also my learning of the technology it is built on.</p>
+            <h2>First Post (TEST)</h2>
+            <p>This is someholding text for what will become the first blog post about this site.</p>
+            <p>This is the second paragraph.</p>
+
+        </div>
+
+    <!-- TODO The 'skils side bar' is only intended to appear on the CV page -->
+        <aside class="skills_side_bar">
+
+        </aside>
+
+        <?php include("includes/footer.php"); ?>
+
+    </div>
+
+</body>
+</html>
