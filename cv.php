3713564441ad942c4422dd16281381c675949e98
Tony Homfray <tonyhomfray@gmail.com>
2019-05-17 20:45:33 +0100

    Adds the CV page.

---
+++ cv.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>CV</title>
+</head>
+<body>
+    <div class="grid-wrapper">
+
+        <?php include("includes/header-footer.php"); ?>
+
+        <div id="main_section">
+            <h2>Profile</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis deleniti laudantium labore sunt. Rem dolor repellat veritatis laudantium eum ipsum quidem, consequuntur ad fuga amet harum sed quos nisi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur magni minus, veritatis neque natus ullam nobis deleniti. Ratione magnam repudiandae explicabo enim optio, similique laborum corrupti sint, rem totam laboriosam.</p>
+
+            <h2>Current Role</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur optio placeat corrupti quidem ea ipsa ratione iste quia voluptate sint minima aliquid dolorem autem nam, tempora ipsam qui dignissimos quo!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus deserunt distinctio molestiae minima unde odio officia cumque, nesciunt quidem, architecto obcaecati perspiciatis veniam natus inventore voluptate asperiores, aspernatur id blanditiis!</p>
+
+            <h2>Previous Roles</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non aliquid libero explicabo? Quisquam deleniti cum accusamus maxime distinctio maiores, voluptatem cumque enim consectetur, iusto nostrum nulla corrupti ullam doloremque porro.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et iure ab ipsa amet, corrupti libero consectetur, assumenda a atque fuga minima quam tempora suscipit quisquam expedita natus quas alias tenetur.</p>
+
+
+        </div>
+
+    <!-- TODO The 'skils side bar' is only intended to appear on the CV page -->
+        <aside class="skills_side_bar">
+
+        </aside>
+
+        <?php include("includes/footer.php"); ?>
+
+    </div>
+
+</body>
+</html>
