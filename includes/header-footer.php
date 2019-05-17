907d5d49bf01b656c09d19d3654cae5a33c4f8d0
Tony Homfray <tonyhomfray@gmail.com>
2019-05-17 20:48:01 +0100

    Adds some explanatory comments to the header-nav.php file.

--- includes/header-footer.php
+++ includes/header-footer.php
@@ -1,11 +1,13 @@
-<header id="main_header">
-            <span id="header_text">Image Here!</span>
+<!-- Main Header for all pages -->        
+        <header id="main_header">
+            <span id="header_text">Beth</span>
         </header>
 
-
+<!-- Main Nav for all pages -->
         <nav id="main_nav">
             <ul>
-                <li>CV</li>
+                <a href="index.php"><li>Home</li></a>
+                <a href="cv.php"><li>CV</li></a>
                 <li>Blog</li>
                 <li>Featured Features</li>
             </ul>
