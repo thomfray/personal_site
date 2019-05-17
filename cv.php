3d126ec9afb52f6f9c7ba18fda9017c6744d5526
Tony Homfray <tonyhomfray@gmail.com>
2019-05-17 21:39:39 +0100

    Adds some placeholder text to the sidebar in cv.php and uses CSS Grid to move it (roughly) into place. Not complete though.

--- cv.php
+++ cv.php
@@ -12,27 +12,33 @@
 
         <?php include("includes/header-footer.php"); ?>
 
-        <div id="main_section">
-            <h2>Profile</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis deleniti laudantium labore sunt. Rem dolor repellat veritatis laudantium eum ipsum quidem, consequuntur ad fuga amet harum sed quos nisi.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur magni minus, veritatis neque natus ullam nobis deleniti. Ratione magnam repudiandae explicabo enim optio, similique laborum corrupti sint, rem totam laboriosam.</p>
+        <div class="cv-grid">
+            <div id="main_section">
+                <h2>Profile</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis deleniti laudantium labore sunt. Rem dolor repellat veritatis laudantium eum ipsum quidem, consequuntur ad fuga amet harum sed quos nisi.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur magni minus, veritatis neque natus ullam nobis deleniti. Ratione magnam repudiandae explicabo enim optio, similique laborum corrupti sint, rem totam laboriosam.</p>
 
-            <h2>Current Role</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur optio placeat corrupti quidem ea ipsa ratione iste quia voluptate sint minima aliquid dolorem autem nam, tempora ipsam qui dignissimos quo!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus deserunt distinctio molestiae minima unde odio officia cumque, nesciunt quidem, architecto obcaecati perspiciatis veniam natus inventore voluptate asperiores, aspernatur id blanditiis!</p>
+                <h2>Current Role</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur optio placeat corrupti quidem ea ipsa ratione iste quia voluptate sint minima aliquid dolorem autem nam, tempora ipsam qui dignissimos quo!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus deserunt distinctio molestiae minima unde odio officia cumque, nesciunt quidem, architecto obcaecati perspiciatis veniam natus inventore voluptate asperiores, aspernatur id blanditiis!</p>
 
-            <h2>Previous Roles</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non aliquid libero explicabo? Quisquam deleniti cum accusamus maxime distinctio maiores, voluptatem cumque enim consectetur, iusto nostrum nulla corrupti ullam doloremque porro.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et iure ab ipsa amet, corrupti libero consectetur, assumenda a atque fuga minima quam tempora suscipit quisquam expedita natus quas alias tenetur.</p>
+                <h2>Previous Roles</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non aliquid libero explicabo? Quisquam deleniti cum accusamus maxime distinctio maiores, voluptatem cumque enim consectetur, iusto nostrum nulla corrupti ullam doloremque porro.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et iure ab ipsa amet, corrupti libero consectetur, assumenda a atque fuga minima quam tempora suscipit quisquam expedita natus quas alias tenetur.</p>
 
 
+            </div>
+
+            <aside class="skills_side_bar">
+                <p>My Skills!</p>
+                <ul>
+                    <li>SQL</li>
+                    <li>PHP</li>
+                    <li>HTML/CSS</li>
+                </ul>
+            </aside>
         </div>
-
-    <!-- TODO The 'skils side bar' is only intended to appear on the CV page -->
-        <aside class="skills_side_bar">
-
-        </aside>
-
+        
         <?php include("includes/footer.php"); ?>
 
     </div>
